6dc15e33fe49c857f752755206fa4eff0200a0a8
GaoKai <kxjhaha@vip.qq.com>
2018-03-27 16:32:48 +0800

    第一版

---
+++ APKRepackageSDKTool/APKRepackageSDKTool/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APKRepackageSDKTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 点击事件
+
+        private void Button_ClickSelectAPK(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("点击了click事件");
+
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "APK Files (*.apk)|*.apk"
+            };
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                this.Text_APKPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_ClicRepackageAPK(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+
+            p.StandardInput.WriteLine("ipconfig");
+
+            p.StandardInput.WriteLine("exit");
+
+            p.StandardInput.AutoFlush = true;
+
+            string output = p.StandardOutput.ReadToEnd();
+
+            Text_output.Text = output;
+
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+        }
+
+        #endregion
+    }
+}
