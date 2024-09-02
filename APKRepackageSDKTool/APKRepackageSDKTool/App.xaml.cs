af88d6233db37f69035e08fef1aba737cdfc74ff
kisence <kisence@cdmosstech.com>
2024-09-02 18:49:26 +0800

    提交构建文件

--- APKRepackageSDKTool/APKRepackageSDKTool/App.xaml.cs
+++ APKRepackageSDKTool/APKRepackageSDKTool/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Shapes;
 
 namespace APKRepackageSDKTool
 {
@@ -13,5 +14,6 @@ namespace APKRepackageSDKTool
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
